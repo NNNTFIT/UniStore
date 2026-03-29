d4f39fde8ac61288a555fce1890faf92c997a071
tuankietptk1507-commits <tuankietptk1507@gmail.com>
2026-03-29 19:25:45 +0700

    1

--- share/js/header.js
+++ share/js/header.js
@@ -1,4 +1,24 @@
-let authHTML = `        <div class="header-container">
+function renderHeader() {
+    const loggedInUser = JSON.parse(localStorage.getItem("user"));
+    let authContent = "";
+    if (loggedInUser) {
+        authContent = `
+            <a href="../../pages/upload/upload.html" class="header-nav-upload">Tải App lên</a>
+            <div class="user-profile">
+                <img src="${loggedInUser.avatar}" alt="Avatar" class="user-avatar" onclick="toggleDropdown()">
+                <div id="user-dropdown" class="dropdown-content">
+                    <a href="#">Tài khoản</a>
+                    <a href="#" onclick="handleLogout()">Đăng xuất</a>
+                </div>
+            </div>`;
+    } else {
+        authContent = `
+            <a href="../../pages/login/login.html" class="header-nav-login">Đăng nhập</a>
+            <a href="../../pages/upload/upload.html" class="header-nav-upload">Tải App lên</a>`;
+    }
+
+    let headerHTML = `
+        <div class="header-container">
             <a href="../../pages/home/index.html" class="logo">
                 <img src="../../share/img/logo.svg" alt="">
                 <p>UniStore</p>
@@ -11,8 +31,10 @@ let authHTML = `        <div class="header-container">
                 <input type="text" placeholder="Tìm kiếm ứng dụng">
             </div>
             <div class="header-nav-btn">
-                <a href="../../pages/login/login.html" class="header-nav-login">Đăng nhập</a>
-                <a href="../../pages/upload/upload.html" class="header-nav-upload">Tải App lên</a>
+                ${authContent}
             </div>
-        </div>`
-document.getElementsByTagName("header")[0].innerHTML = authHTML;
+        </div>`;
+
+    document.getElementsByTagName("header")[0].innerHTML = headerHTML;
+}
+renderHeader();
